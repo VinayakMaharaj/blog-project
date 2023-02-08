522f7a1741bcdb149dc8e28d0faa06c3258e9c20
Vinayak <vinayakcpa@gmail.com>
2023-02-08 12:02:35 -0500

    Added in the Header and Layout React components with relevant exports in the index.js file

---
+++ components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "./";
+
+export const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
